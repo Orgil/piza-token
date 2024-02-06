e018dae3d96d4f9d51b531adc6f067226248f954
Orgil <orgil.u@gmail.com>
2024-02-06 19:45:28 +0800

    added test and natspec doc

--- contracts/PizaToken.sol
+++ contracts/PizaToken.sol
@@ -3,26 +3,55 @@ pragma solidity ^0.8.20;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol';
+import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
 import './Blacklistable.sol';
 
-contract PizaToken is ERC20, ERC20Pausable, Blacklistable {
+/**
+ * @title An ERC20 token contract named Piza
+ * @author Orgil
+ * @notice Serves as a fungible utility token
+ * @dev Inherits the OpenZepplin ERC20, ERC20Burnable, ERC20Pausable implentations
+ */
+contract PizaToken is ERC20, ERC20Burnable, ERC20Pausable, Blacklistable {
+  /**
+   * @notice Deploys the smart contract and creates 5000000000 tokens
+   * @dev Assigns `contractOwner` to the owner state variable
+   * @param contractOwner The owner of the deployed token contract
+   * @param tokenOwner The amount of tokens `tokenOwner` will receive
+   */
   constructor(address contractOwner, address tokenOwner) ERC20('Piza Token', 'PIZA') Ownable(contractOwner) {
     _mint(tokenOwner, 5000000000 * 10 ** decimals());
   }
 
+  /**
+   * @notice Owner of the contract can pause the all trades
+   * @dev Owner of the contract can pause the all trades
+   */
   function pause() public onlyOwner {
     _pause();
   }
 
+  /**
+   * @notice Owner of the contract can pause the all trades
+   * @dev Owner of the contract can pause the all trades
+   */
   function unpause() public onlyOwner {
     _unpause();
   }
 
-  // The following functions are overrides required by Solidity.
+  /**
+   * @dev The following functions are overrides required by Solidity.
+   */
   function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Pausable) {
     super._update(from, to, value);
   }
 
+  /**
+   * @notice Transfer tokens to address
+   * @dev Overrides transfer function to check blacklist before transfer occurs
+   * @param to address The address which you want to transfer to
+   * @param amount uint256 the amount of tokens to be transferred
+   */
   function transfer(
     address to,
     uint amount
@@ -30,6 +59,13 @@ contract PizaToken is ERC20, ERC20Pausable, Blacklistable {
     return super.transfer(to, amount);
   }
 
+  /**
+   * @notice Transfer tokens from one address to another
+   * @dev Overrides transferFrom function to check blacklist before transfer occurs
+   * @param from address The address which you want to send tokens from
+   * @param to address The address which you want to transfer to
+   * @param amount uint256 the amount of tokens to be transferred
+   */
   function transferFrom(
     address from,
     address to,
--- test/PizaToken.test.ts
+++ test/PizaToken.test.ts
@@ -1,1 +1,110 @@
 // SPDX-License-Identifier: MIT
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { PizaToken } from '../typechain-types';
+import { ContractRunner } from 'ethers';
+import { HardhatEthersSigner } from '@nomicfoundation/hardhat-ethers/signers';
+
+const fixture = async () => {
+  const [contractOwner, tokenOwner, recipient, approved] = await ethers.getSigners();
+
+  const deployment = await ethers.deployContract('PizaToken', [contractOwner, tokenOwner]);
+  await deployment.waitForDeployment();
+  return { contractOwner, tokenOwner, recipient, approved, deployment };
+};
+
+let g: {
+  contractOwner: ContractRunner;
+  tokenOwner: HardhatEthersSigner;
+  recipient: HardhatEthersSigner;
+  approved: HardhatEthersSigner;
+  deployment: PizaToken;
+};
+
+describe('Piza Token', () => {
+  beforeEach(async () => {
+    g = await loadFixture(fixture);
+  });
+
+  it('Deployment should mint the total supply of tokens to the token owner', async function () {
+    const ownerBalance = await g.deployment.balanceOf(g.tokenOwner.address);
+    expect(await g.deployment.totalSupply()).to.equal(ownerBalance);
+  });
+
+  describe('blacklist', () => {
+    it('account is blacklisted', async () => {
+      await g.deployment.addToBlacklist(g.recipient);
+      expect(await g.deployment.connect(g.tokenOwner).isBlacklisted(g.recipient)).to.be.true;
+    });
+
+    it('account is not blacklisted', async () => {
+      expect(await g.deployment.connect(g.tokenOwner).isBlacklisted(g.recipient)).to.be.false;
+    });
+
+    it('account is not blacklisted after remove from blacklist', async () => {
+      await g.deployment.addToBlacklist(g.recipient);
+      await g.deployment.removeFromBlacklist(g.recipient);
+
+      expect(await g.deployment.connect(g.tokenOwner).isBlacklisted(g.recipient)).to.be.false;
+    });
+  });
+
+  describe('transfer', () => {
+    const transfer = 100n;
+
+    it('allow transfer', async () => {
+      await expect(g.deployment.connect(g.tokenOwner).transfer(g.recipient, transfer)).to.changeTokenBalances(
+        g.deployment,
+        [g.tokenOwner, g.recipient],
+        [-transfer, transfer]
+      );
+    });
+
+    it('reverts transfer FROM blacklisted account', async () => {
+      await g.deployment.addToBlacklist(g.tokenOwner);
+
+      await expect(g.deployment.connect(g.tokenOwner).transfer(g.recipient, transfer)).to.be.revertedWith(
+        'Address is blacklisted'
+      );
+    });
+
+    it('reverts transfer TO blacklisted account', async () => {
+      await g.deployment.addToBlacklist(g.recipient);
+
+      await expect(g.deployment.connect(g.tokenOwner).transfer(g.recipient, transfer)).to.be.revertedWith(
+        'Address is blacklisted'
+      );
+    });
+  });
+
+  describe('transferFrom', () => {
+    const allowance = 100n;
+
+    beforeEach(async function () {
+      await g.deployment.connect(g.tokenOwner).approve(g.approved, allowance);
+    });
+
+    it('allow transferFrom', async () => {
+      await expect(
+        g.deployment.connect(g.approved).transferFrom(g.tokenOwner, g.recipient, allowance)
+      ).to.changeTokenBalances(g.deployment, [g.tokenOwner, g.recipient], [-allowance, allowance]);
+    });
+
+    it('reverts transferFrom FROM blacklisted account', async () => {
+      await g.deployment.addToBlacklist(g.tokenOwner);
+
+      await expect(
+        g.deployment.connect(g.approved).transferFrom(g.tokenOwner, g.recipient, allowance)
+      ).to.be.revertedWith('Address is blacklisted');
+    });
+
+    it('reverts transferFrom TO blacklisted account', async () => {
+      await g.deployment.addToBlacklist(g.recipient);
+
+      await expect(
+        g.deployment.connect(g.approved).transferFrom(g.tokenOwner, g.recipient, allowance)
+      ).to.be.revertedWith('Address is blacklisted');
+    });
+  });
+});
